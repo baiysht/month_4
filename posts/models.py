3b5038c860074766261f7c517097c208384b8948
baiysht <torobekovb2010@gmail.com>
2025-01-13 22:15:06 +0600

    add:HW-3

--- posts/models.py
+++ posts/models.py
@@ -2,12 +2,32 @@ from symtable import Class
 from django.db import models
 
 
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.title}'
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.title}'
+
+
 class Post(models.Model):
+    image = models.ImageField(upload_to='images/', null=True, blank=True)
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=256)
     content = models.CharField(max_length=256, null=True)
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     updated_at = models.DateTimeField(auto_now=True, null=True)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, related_name='posts', null=True, blank=True)
+    tag = models.ManyToManyField('Tag', related_name='posts', blank=True, null=True)
+
 
     def __str__(self):
         return f'{self.title} - {self.content} - {self.description}'
+
+
--- posts/views.py
+++ posts/views.py
@@ -10,3 +10,8 @@ def main_view(request):
 def posts_list_view(request):
     posts = Post.objects.all()
     return render(request, "posts_list.html", context={"posts": posts})
+
+
+def post_detail_view(request, post_id):
+    post = Post.objects.get(id=post_id)
+    return render(request, "post_detail.html", context={"post": post})
